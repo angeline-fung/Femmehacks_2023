0cc601f8fa60d950e6d5a81a84620f6cd25c2d77
mayhathaway <mayhath@gmail.com>
2023-02-11 17:43:04 -0500

    scanner page

--- femmehacks_2023/src/components/App.js
+++ femmehacks_2023/src/components/App.js
@@ -1,24 +1,20 @@
-
-import '../assets/App.css';
+import "../assets/App.css";
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 // import { useState } from 'react';
 
-
-import Landing from './Landing.js';
-import Scanner from './Scanner.js';
-import Items from './Items.js';
+import Landing from "./Landing.js";
+import Scanner from "./Scanner.js";
+import Items from "./Items.js";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Landing />} />
-        {/* <Route path="/scanner" element={<Scanner />} />
-        <Route path="/items" element={<Items />} /> */}
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/home" element={<Landing />} />
+      <Route path="/scanner" element={<Scanner />} />
+      <Route path="/items" element={<Items />} />
     </Routes>
-
-
   );
 }
 
